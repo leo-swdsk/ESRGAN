b150aa506f90186c190874c95f99f319e9874cc3
niko_swds <nikolassawadski@gmail.com>
2025-09-09 03:20:43 +0200

    Löschen unnötiger Dateien, gleich wird neues Modell trainiert

--- RRDBNet_arch.py
+++ RRDBNet_arch.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+#architecture of the original ESRGAN model
 def make_layer(block, n_layers):
     layers = []
     for _ in range(n_layers):
